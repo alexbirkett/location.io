49572528fa990a860054db66260d8d855d92569c
Alex Birkett <alex@birkett.co.uk>
2012-11-06 09:45:28 +0100

    remove capabilities from handler file make tracker id simpler (just use protocol name and id)

--- protocol/gotop/handler.js
+++ protocol/gotop/handler.js
@@ -1,87 +1,6 @@
 var goTopMessageParser = require('./parser');
 var events = require('events');
 
-var capabilities = {
-		commands : [ {
-			name : "oneTimeLocate",
-			passwordRequired : true
-		} ],
-		properties : [ {
-			name : "authorizedNumber",
-			count:5,
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "telephoneNumber",
-				name : "authorizedNumber"
-			} ]
-		}, {
-			name : "continous-tracking",
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "time-period",
-				name : "update-fequency"
-			} ]
-		},{
-			name : "speeding-alarm",
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "boolean",
-				name : "enabled"
-			},{
-				type : "speed",
-				name : "speed"
-			} ]
-		},{
-			name : "geoFence",
-			count: 5,
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "longitude",
-				name : "minLongitude"
-			}, {
-				type : "longitude",
-				name : "maxLongitude"
-			}, {
-				type : "latitude",
-				name : "minLatitude"
-			}, {
-				type : "latitude",
-				name : "maxLatitude"
-			} ]
-		},{
-			name : "timeZone",
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "timezone",
-				name : "timezone"
-			}]
-		},{
-			name : "lowBatteryAlarm",
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "boolean",
-				name : "enabled"
-			},{
-				type : "percentage",
-				name : "percentage"
-			}]
-		},{
-			name : "changePassword",
-			writable:true,
-			passwordRequired:true,
-			parameters : [ {
-				type : "password",
-				name : "password"
-			}]
-		}]
-	};
-
 var GoTopProtocolHandler = function(eventEmitter) {
 	this.eventEmitter = eventEmitter;
 };
@@ -172,13 +91,13 @@ GoTopProtocolHandler.prototype.sendCommand = function(command, callback) {
 };
 
 GoTopProtocolHandler.prototype.getId = function() {
-	return 'gotop' + this.socket.remoteAddress + 'X' + this.socket.remotePort + 'X' + this.id;
+	return 'gotop' + this.id;
 };
 
 GoTopProtocolHandler.prototype.setAndEmittIdIfrequired = function(message) {
 	if (this.id == undefined) {
 		this.id = message.serialNumber;
-		this.eventEmitter.emit('tracker-connected', { id:this.getId(),capabilities: capabilities }, this);
+		this.eventEmitter.emit('tracker-connected', { id:this.getId(),protocol: "gotop" }, this);
 	}
 };
 
