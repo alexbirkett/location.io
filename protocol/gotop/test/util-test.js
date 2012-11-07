ec2011d56baa00bd4eb59081fc05ee8529a587ba
Alex Birkett <alex@birkett.co.uk>
2012-11-07 13:52:33 +0100

    use decimal minutes (not minutes and seconds)

--- protocol/gotop/test/util-test.js
+++ protocol/gotop/test/util-test.js
@@ -3,13 +3,16 @@ var util = require('../util');
 var assert = require('assert');
 
 var testFormatLongitude = function() {
-	assert.equal(util.formatLongitude(-1.234), "0011402W");
-	assert.equal(util.formatLongitude(45.563), "0453347E");
+	// -001°14.07960
+	assert.equal(util.formatLongitude(-1.23466), "0011408W");
+	// 045°33.78000'
+	assert.equal(util.formatLongitude(45.563), "0453378E");
 };
 
 var testFormatLatitude = function() {
 	assert.equal(util.formatLatitude(89.8), "894800N");
-	assert.equal(util.formatLatitude(-12.744), "124438S");
+	// 12°44.64000
+	assert.equal(util.formatLatitude(-12.744), "124464S");
 };
 
 testFormatLongitude();
