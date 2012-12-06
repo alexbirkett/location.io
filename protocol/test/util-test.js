e98f1979c119d72e0fa3782f09bdab9b8be3678b
Alex Birkett <alex@birkett.co.uk>
2012-12-06 10:34:12 +0100

    convert utils tests to use vows

--- protocol/test/util-test.js
+++ protocol/test/util-test.js
@@ -1,37 +1,56 @@
 var util = require('../util');
 
 var assert = require('assert');
+var vows = require('vows');
 
-var testParseTimeInterval = function() {
-	
-	
-	var seconds = util.parseTimeInterval('1s');
-	
-	assert.equal(seconds, 1);
-	
-	var seconds = util.parseTimeInterval('30S');
-	
-	assert.equal(seconds, 30);
-	
-	var seconds = util.parseTimeInterval('1m');
-	
-	assert.equal(seconds, 60);
-	
-	var seconds = util.parseTimeInterval('2M');
-	
-	assert.equal(seconds, 120);
-	
-	var seconds = util.parseTimeInterval('1H');
-	
-	assert.equal(seconds, 3600);
-	
+vows.describe('buffer').addBatch({
+    'handles 1s': {
+        topic: function() {
+        	return util.parseTimeInterval('1s');
+        },
+        'should be 1': function (seconds) {
+    		 assert.equal(seconds, 1);
+        }
+    },
+    'handles 30s': {
+        topic: function() {
+        	return util.parseTimeInterval('30S');
+        },
+        'should be 30': function (seconds) {
+          assert.equal(seconds, 30);
+        }
+    },
+    'handles 1m': {
+        topic: function() {
+        	return util.parseTimeInterval('1m');
+        },
+        'should be 60': function (seconds) {
+          	assert.equal(seconds, 60);
+        }
+    },
+    'handles 2M': {
+        topic: function() {
+        	return util.parseTimeInterval('2M');
+        },
+        'should be 30': function (seconds) {
+          	assert.equal(seconds, 120);
+        }
+    },
+    'handles 1H': {
+        topic: function() {
+        	return util.parseTimeInterval('1H');
+        },
+        'should be 3600': function (seconds) {
+          	assert.equal(seconds, 3600);
+        }
+    },
+     'handles 2H': {
+        topic: function() {
+        	return  util.parseTimeInterval('2h');
+        },
+        'should be 7200': function (seconds) {
+          	assert.equal(seconds, 7200);
+        }
+    }
+}).export(module); // Export the Suite
 
-	var seconds = util.parseTimeInterval('2h');
-	
-	assert.equal(seconds, 7200);
-	
-};
-
-testParseTimeInterval();
-
-console.log('done');
