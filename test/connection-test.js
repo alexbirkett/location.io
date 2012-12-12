b02dd3498013e33d38023fd789524bc0322aa497
Alex Birkett <alex@birkett.co.uk>
2012-12-12 12:37:55 +0100

    added test parse with one module that returns a error and none that return a message

--- test/connection-test.js
+++ test/connection-test.js
@@ -29,14 +29,46 @@ vows.describe('connection.parse').addBatch({
 			var connection = new Connection(function() {
 			}, protocolModules);
 			connection._parse(new Buffer(0), this.callback);
-			console.log('hello');
 		},
 		'should removesModules all other modules if a module successfully parses message' : function(err, message, data, connection) {
-			console.log('test');
 			assert.isNull(err);
 			assert.equal(message, "message");
 			assert.equal(data.length, 0);
 			assert.equal(connection.protocolModules.length, 1);
+		}
+	},
+	'parse with one module that returns a error and none that return a message' : {
+		topic : function() {
+			var protocolModules = [{
+				parse : function(buffer, callback) {
+					process.nextTick(function() {
+						callback("error", null, buffer);
+					});
+				}
+			}, {
+				parse : function(buffer, callback) {
+					process.nextTick(function() {
+						callback(null, null, buffer);
+					});
+				}
+			}, {
+				parse : function(buffer, callback) {
+					process.nextTick(function() {
+						callback(null, null, buffer);
+					});
+				},
+				desiredModule : true
+			}];
+
+			var connection = new Connection(function() {
+			}, protocolModules);
+			connection._parse(new Buffer(7), this.callback);
+		},
+		'should removesModules all other modules if a module successfully parses message' : function(err, message, data, connection) {
+			assert.isNull(err);
+			assert.isNull(message); // no message was sent back
+			assert.equal(data.length, 7); // we get back the same buffer we passed in
+			assert.equal(connection.protocolModules.length, 2); // the module that threw an error should have been removed
 		}
 	}
 }).export(module);
