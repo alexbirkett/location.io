e71101bb6cadba4aa15519db73646870262cde7f
Alex Birkett <alex@birkett.co.uk>
2012-12-19 15:30:28 +0100

    move parseTimeInterval tests into parseTimeInterval block

--- protocol/test/util-test.js
+++ protocol/test/util-test.js
@@ -3,54 +3,58 @@ var util = require('../util');
 var assert = require('assert');
 var vows = require('vows');
 
-vows.describe('buffer').addBatch({
-    'handles 1s': {
-        topic: function() {
-        	return util.parseTimeInterval('1s');
-        },
-        'should be 1': function (seconds) {
-    		 assert.equal(seconds, 1);
-        }
-    },
-    'handles 30s': {
-        topic: function() {
-        	return util.parseTimeInterval('30S');
-        },
-        'should be 30': function (seconds) {
-          assert.equal(seconds, 30);
-        }
-    },
-    'handles 1m': {
-        topic: function() {
-        	return util.parseTimeInterval('1m');
-        },
-        'should be 60': function (seconds) {
-          	assert.equal(seconds, 60);
-        }
-    },
-    'handles 2M': {
-        topic: function() {
-        	return util.parseTimeInterval('2M');
-        },
-        'should be 30': function (seconds) {
-          	assert.equal(seconds, 120);
-        }
-    },
-    'handles 1H': {
-        topic: function() {
-        	return util.parseTimeInterval('1H');
-        },
-        'should be 3600': function (seconds) {
-          	assert.equal(seconds, 3600);
-        }
-    },
-     'handles 2H': {
-        topic: function() {
-        	return  util.parseTimeInterval('2h');
-        },
-        'should be 7200': function (seconds) {
-          	assert.equal(seconds, 7200);
-        }
-    }
-}).export(module); // Export the Suite
+vows.describe('utils').addBatch({
+	'parseTimeInterval' : {
+		'handles 1s' : {
+			topic : function() {
+				return util.parseTimeInterval('1s');
+			},
+			'should be 1' : function(seconds) {
+				assert.equal(seconds, 1);
+			}
+		},
+		'handles 30s' : {
+			topic : function() {
+				return util.parseTimeInterval('30S');
+			},
+			'should be 30' : function(seconds) {
+				assert.equal(seconds, 30);
+			}
+		},
+		'handles 1m' : {
+			topic : function() {
+				return util.parseTimeInterval('1m');
+			},
+			'should be 60' : function(seconds) {
+				assert.equal(seconds, 60);
+			}
+		},
+		'handles 2M' : {
+			topic : function() {
+				return util.parseTimeInterval('2M');
+			},
+			'should be 30' : function(seconds) {
+				assert.equal(seconds, 120);
+			}
+		},
+		'handles 1H' : {
+			topic : function() {
+				return util.parseTimeInterval('1H');
+			},
+			'should be 3600' : function(seconds) {
+				assert.equal(seconds, 3600);
+			}
+		},
+		'handles 2H' : {
+			topic : function() {
+				return util.parseTimeInterval('2h');
+			},
+			'should be 7200' : function(seconds) {
+				assert.equal(seconds, 7200);
+			}
+		}
+	},
+	
+}).export(module);
+// Export the Suite
 
