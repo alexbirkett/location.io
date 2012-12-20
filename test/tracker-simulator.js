3437a38a7046e3223ca54a789255f592eb3f2686
Alex Birkett <alex@birkett.co.uk>
2012-12-20 14:14:43 +0100

    tracker-simulator and test case

---
+++ test/tracker-simulator.js
@@ -0,0 +1,85 @@
+var net = require('net');
+var async = require('async');
+var forEach = require('async-foreach').forEach;
+
+function sliceString(string, sliceLength, index) {
+	console.log('string length ' + string.length + ' slice length' + sliceLength  + 'index  ' + index);
+	var beginPos = index*sliceLength;
+	var endPos = beginPos + sliceLength;
+	
+	console.log('begin pos ' + beginPos + ' end pos ' + endPos);
+	var slice;
+	if (beginPos < string.length) {
+		if (endPos < string.length) {
+			slice = string.slice(beginPos, beginPos + sliceLength);
+		} else {
+			slice = string.slice(beginPos);
+		}
+	} else {
+		throw "out of bounds";
+	}
+	return slice;
+}
+
+var SLICE_LENGTH = 1;
+function buildSliceArray(message) {
+	var slices = [];
+	for(var i = 0;true;i++) {
+		try {
+			var slice = sliceString(message, SLICE_LENGTH, i);
+			slices.push(slice);
+		} catch(e) {
+			break;
+		}
+	}
+	return slices;
+}
+
+function sendMessage(message, client, pauseBetweenSlices, callback) {
+	var slices = buildSliceArray(message);
+	var sendNextSlice = function(slice, sendSliceCallback) {
+		client.write(slice);
+		setTimeout(function() {
+			sendSliceCallback();
+		}, pauseBetweenSlices);
+		
+	};
+
+	async.forEachSeries(slices, sendNextSlice, function() {
+		callback();
+	});
+}
+
+function createEvent(name) {
+	event = new Object();
+	event.name = name;
+	return event;
+}
+
+var client;
+
+module.exports.connect = function(connectOptions, callback) {
+	client = net.createConnection(connectOptions, callback);
+}
+
+module.exports.sendMessage = function(messages, pauseBetweenMessages, pauseBetweenSlices, callback) {
+
+	forEach(messages, function(message, index, arr) {
+		var done = this.async();
+
+		sendMessage(message, client, pauseBetweenSlices, function() {
+			setTimeout(function() {
+				done();
+			}, pauseBetweenMessages);
+		});
+
+
+	}, function() {
+		callback();
+	});
+}; 
+
+module.exports.destroy = function() {
+	client.destroy();
+}
+	
