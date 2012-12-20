0f2f906a2a9a9d8e0733e76ab58ac23cd7f6d65a
Alex Birkett <alex@birkett.co.uk>
2012-12-20 15:25:02 +0100

    added protocol-identifier-test that creates server and connects to it using simulated tracker over tcp

--- test/protocol-identifier-test.js
+++ test/protocol-identifier-test.js
@@ -1,29 +1,54 @@
+
 var assert = require('assert');
+var vows = require('vows');
+var trackerSimulator = require('./tracker-simulator');
+var async = require('async');
 
-var ProtocolIdentifer = require('../protocol-identifier');
+var tk103Buffer = "(012345678901BO012110601V5955.9527N01047.4330E000.023100734.62000000000L000000)";
+var gotTopMessage = "#353327020115804,CMD-T,A,DATE:090329,TIME:223252,LAT:22.7634066N,LOT:114.3964783E,Speed:000.0,84-20,#";
 
-var phonelocator = function() {
-	
-};
+var LocationIo = require('../index');
 
-var goTop = function() {
-	console.log('goTop');
-};
+var locationIo = new LocationIo();
 
-var tk103 = function() {
-	console.log('tk103');
-};
+var nextPort = 11235;
+var getNextPort = function() {
+	return nextPort++;
+}
 
-
-
-protocolIdentifer = new ProtocolIdentifer(phonelocator, goTop, tk103);
-
-var tk103Buffer = new Buffer("(012345678901BO012110601V5955.9527N01047.4330E000.023100734.62000000000L000000)");
-var gotTopBuffer = new Buffer("#353327020115804,CMD-T,A,DATE:090329,TIME:223252,LAT:22.7634066N,LOT:114.3964783E,Speed:000.0,84-20,#");
-
-assert.equal(tk103, protocolIdentifer.identifyProtocol(tk103Buffer), "tk103 failed");
-assert.equal(goTop, protocolIdentifer.identifyProtocol(gotTopBuffer), "goTop failed");
-
-console.log('tests complete');
-
-
+vows.describe('protocol-identifier-tests').addBatch({
+    'handles gotop message': {
+        topic: function() {
+        	var callback = this.callback;
+        	var port = getNextPort();
+        	var protocolType;
+			locationIo.createServer(port, function(eventType, id, message) {
+				//var eventAruments = arguments;
+				console.log('event type ' + eventType);
+				if (eventType == 'tracker-connected') {
+					protocolType = message;
+				} else if (eventType == 'message') {
+					locationIo.close(function() {
+						callback(id, message, protocolType);
+					});
+				} else if (eventType == 'server-up') {
+					async.series([
+			   			function(callback)	{
+			   				trackerSimulator.connect({host: 'localhost', port: port}, callback);
+			   			},
+			    		function(callback) {
+			    			var messages = [gotTopMessage];
+			    			trackerSimulator.sendMessage(messages, 1000, 2, callback);
+			    		}
+					],function(err) {
+						trackerSimulator.destroy();
+					});
+				}
+			});
+        },
+        'should be gotop message': function (id, message, protocol) {
+			assert.equal(protocol, "gotop");
+        }
+    },
+  
+}).export(module); // Export the Suite
