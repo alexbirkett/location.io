4fa8f27c63c6310e7b56d5d86e5de2b00c9b3334
Alex Birkett <alex@birkett.co.uk>
2012-12-22 17:58:36 +0100

    make parser asynchronous

--- protocol/tk103/parser.js
+++ protocol/tk103/parser.js
@@ -155,10 +155,9 @@ var lookupMessageType = function(code) {
 
 //var answerDeviceLoginResponsePattern = /(\d+)(\d{6}[AV].+)/;
 
-function parseMessage(message, result) {
+function parseMessage(message) {
 	// we can't use regex here because we need to be able to pass the message body as a buffer to the parse function
 	var frame = {};
-	result.frame = frame;
 	
 	var trackerIdlength = 0;
 	while(message.readInt8(trackerIdlength) != 66 && trackerIdlength < message.length) { // 66 is B
@@ -171,7 +170,6 @@ function parseMessage(message, result) {
 	
 	frame.type = messageType[0];
 	var parseFunction = messageType[1];
-	result.requiredMessageAck = messageType[2];
 	
 	if (parseFunction != undefined) {
 		var messageBody = message.slice(trackerIdlength + 4);
@@ -181,28 +179,34 @@ function parseMessage(message, result) {
 	return frame;
 }
 
-
-var findFrameAndParseMessage = function(buffer) {
-	var result = {};
-	result.buffer = buffer;
+var findFrameAndParseMessage = function(buffer, callback) {
 	// pass back the buffer if we can't parse the message yet
 	var messageStartIndex = -1;
-
-	for (var i = 0; i < buffer.length; i++) {
-
-		var charValue = buffer.readUInt8(i)
-
-		if (charValue == 41) {
-			result.message = parseMessage(buffer.slice(messageStartIndex + 1, i), result);
-			result.buffer = buffer.slice(i + 1);
-			break;
-		} else if (charValue == 40) {
-			messageStartIndex = i;
+	var message;
+	var bufferToReturn = buffer;
+	var error = null;
+	try {
+		if (buffer.length > 0 && buffer.readUInt8(0) == 40) {
+			for (var i = 1; i < buffer.length; i++) {
+				var charValue = buffer.readUInt8(i)
+		
+				if (charValue == 41) {
+					message = parseMessage(buffer.slice(messageStartIndex + 1, i));
+					bufferToReturn = buffer.slice(i + 1);
+					break;
+				}
+			}
+		} else {
+			error = "incompatible protocol";
 		}
+	} catch (e) {
+		error;
 	}
-	return result;
+	
+	process.nextTick(function() {
+		callback(error, message, bufferToReturn);
+	});
 
 }
-
 
 module.exports = findFrameAndParseMessage;
