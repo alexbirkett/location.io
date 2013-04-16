113e62ff902e457153cb67671263e31734598a30
Alex Birkett <alex@birkett.co.uk>
2013-04-16 14:32:08 +0200

    Move buildAck method into message-builder.js

--- protocol/tk103/message-builder.js
+++ protocol/tk103/message-builder.js
@@ -7,86 +7,92 @@ var buildsetAccSendingDataIntervals = function(messageValue, commandId) {
     var time = getTimeIntervalAsFourHexDigits(util.parseTimeInterval(messageValue.interval));
     return buildSimpleCommand(messageValue, commandId + time);
 }
-
 var alarmTypes = {
-    'vehiclePowerOff':'0',
-    'accident':'1',
-    'sos':'2',
-    'vehicleAlarm':'3',
-    'underSpeed':'4',
-    'overSpeed':'5',
-    'geoFence': '6',
-    'movement':'7'
+    'vehiclePowerOff' : '0',
+    'accident' : '1',
+    'sos' : '2',
+    'vehicleAlarm' : '3',
+    'underSpeed' : '4',
+    'overSpeed' : '5',
+    'geoFence' : '6',
+    'movement' : '7'
 }
 
-module.exports = {
-    messages : {
-        configureUpdateInterval : function(messageValue) {
-            var intervalInSeconds = messageValue.enabled ? util.parseTimeInterval(messageValue.interval) : 0;
-            var durationInSeconds = messageValue.enabled ? util.parseTimeInterval(messageValue.duration) : 0;
-            var durationHours = util.prependZeros(Math.floor(durationInSeconds / 3600).toString(16), 2).toUpperCase();
-            var durationMinutes = util.prependZeros(Math.floor(durationInSeconds / 60).toString(16), 2).toUpperCase();
-            return "(" + messageValue.trackerId + "AR00" + util.getTimeIntervalAsFourHexDigits(intervalInSeconds) + durationHours + durationMinutes + ")";
-        },
-        requestLocation : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AP00");
-        },
-        configureSpeedAlert : function(messageValue) {
-            if (messageValue.maxSpeed > 999) {
-                throw new Error("max speed must be less than 999 km/h");
-            }
-
-            if (messageValue.minSpeed > 999) {
-                throw new Error("min speed must be less than 999 km/h");
-            }
-            return "(" + messageValue.trackerId + "AP12H" + util.prependZeros(messageValue.maxSpeed, 3) + "L" + util.prependZeros(messageValue.minSpeed, 3) + ")";
-        },
-        configureSwitch0 : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AV00" + (messageValue.enabled ? "1" : "0"));
-        },
-        configureSwitch1 : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AV01" + (messageValue.enabled ? "1" : "0"));
-        },
-        restartDevice : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AT00");
-        },
-        configureUpdateIntervalWhenAccOpen : function(messageValue) {
-            return buildsetAccSendingDataIntervals(messageValue, "AR05");
-        },
-        configureUpdateIntervalWhenAccClosed : function(messageValue) {
-            return buildsetAccSendingDataIntervals(messageValue, "AR06");
-        },
-        configureGeofence : function(messageValue) {
-            var formatLatLng = function(latlng) {
-                return latlng.degrees + latlng.minutes;
-            };
-
-            var parsedMaxLongitude = util.parseLongitudeMinDec(messageValue.maxLongitude, 3);
-            var parsedMinLongitude = util.parseLongitudeMinDec(messageValue.minLongitude, 3);
-            var parsedMaxLatitude = util.parseLatitudeMinDec(messageValue.maxLatitude, 3);
-            var parsedMinLatitude = util.parseLatitudeMinDec(messageValue.minLatitude, 3);
-
-            var messageBody = (messageValue.enabled ? "1" : "0") + ',' + parsedMaxLatitude.hemisphere + "," + +formatLatLng(parsedMinLatitude) + "," + formatLatLng(parsedMaxLatitude) + "," + parsedMinLongitude.hemisphere + "," + +formatLatLng(parsedMinLongitude) + "," + formatLatLng(parsedMaxLongitude);
-            return buildSimpleCommand(messageValue, "AX05" + messageBody);
-        }
+var messages = {
+    configureUpdateInterval : function(messageValue) {
+        var intervalInSeconds = messageValue.enabled ? util.parseTimeInterval(messageValue.interval) : 0;
+        var durationInSeconds = messageValue.enabled ? util.parseTimeInterval(messageValue.duration) : 0;
+        var durationHours = util.prependZeros(Math.floor(durationInSeconds / 3600).toString(16), 2).toUpperCase();
+        var durationMinutes = util.prependZeros(Math.floor(durationInSeconds / 60).toString(16), 2).toUpperCase();
+        return "(" + messageValue.trackerId + "AR00" + util.getTimeIntervalAsFourHexDigits(intervalInSeconds) + durationHours + durationMinutes + ")";
     },
-    acks : {
-        handshakeSignalMessage : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AP01HSO");
-        },
-        loginMessage : function(messageValue) {
-            return buildSimpleCommand(messageValue, "AP05");
-        },
-        alarmMessage : function(messageValue) {
-            var alarmType = alarmTypes[messageValue.alarmType];
-            if (alarmType == undefined) {
-                throw new Error("unknown alarm type");
-            }
-            return "(" + messageValue.trackerId + "AS01" + alarmType + ")";
+    requestLocation : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AP00");
+    },
+    configureSpeedAlert : function(messageValue) {
+        if (messageValue.maxSpeed > 999) {
+            throw new Error("max speed must be less than 999 km/h");
         }
+
+        if (messageValue.minSpeed > 999) {
+            throw new Error("min speed must be less than 999 km/h");
+        }
+        return "(" + messageValue.trackerId + "AP12H" + util.prependZeros(messageValue.maxSpeed, 3) + "L" + util.prependZeros(messageValue.minSpeed, 3) + ")";
+    },
+    configureSwitch0 : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AV00" + (messageValue.enabled ? "1" : "0"));
+    },
+    configureSwitch1 : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AV01" + (messageValue.enabled ? "1" : "0"));
+    },
+    restartDevice : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AT00");
+    },
+    configureUpdateIntervalWhenAccOpen : function(messageValue) {
+        return buildsetAccSendingDataIntervals(messageValue, "AR05");
+    },
+    configureUpdateIntervalWhenAccClosed : function(messageValue) {
+        return buildsetAccSendingDataIntervals(messageValue, "AR06");
+    },
+    configureGeofence : function(messageValue) {
+        var formatLatLng = function(latlng) {
+            return latlng.degrees + latlng.minutes;
+        };
+
+        var parsedMaxLongitude = util.parseLongitudeMinDec(messageValue.maxLongitude, 3);
+        var parsedMinLongitude = util.parseLongitudeMinDec(messageValue.minLongitude, 3);
+        var parsedMaxLatitude = util.parseLatitudeMinDec(messageValue.maxLatitude, 3);
+        var parsedMinLatitude = util.parseLatitudeMinDec(messageValue.minLatitude, 3);
+
+        var messageBody = (messageValue.enabled ? "1" : "0") + ',' + parsedMaxLatitude.hemisphere + "," + +formatLatLng(parsedMinLatitude) + "," + formatLatLng(parsedMaxLatitude) + "," + parsedMinLongitude.hemisphere + "," + +formatLatLng(parsedMinLongitude) + "," + formatLatLng(parsedMaxLongitude);
+        return buildSimpleCommand(messageValue, "AX05" + messageBody);
+    }
+};
+
+var acks = {
+    handshakeSignalMessage : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AP01HSO");
+    },
+    loginMessage : function(messageValue) {
+        return buildSimpleCommand(messageValue, "AP05");
+    },
+    alarmMessage : function(messageValue) {
+        var alarmType = alarmTypes[messageValue.alarmType];
+        if (alarmType == undefined) {
+            throw new Error("unknown alarm type");
+        }
+        return "(" + messageValue.trackerId + "AS01" + alarmType + ")";
     }
 };
 /*exports.configureUpdateInterval = function(messageValue) {
 
  }*/
 
+module.exports.buildAck = function(message) {
+        if (message.type != undefined) {
+            var builder = acks[message.type];
+            if (builder != undefined) {
+                return builder(message);
+            }
+        }
+    }
--- protocol/tk103/module.js
+++ protocol/tk103/module.js
@@ -3,12 +3,5 @@ var messageBuilder = require('./message-builder');
 module.exports = {
 	//commands: require('./commands'),
 	parseMessage: require('./parser'),
-	buildAck: function(message) {
-        if (message.type != undefined) {
-            var builder = messageBuilder.acks[message.type];
-            if (builder != undefined) {
-                return builder(message);
-            }
-        }
-	}
+	buildAck: messageBuilder.buildAck
 };
