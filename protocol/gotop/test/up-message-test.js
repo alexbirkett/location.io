9d6eb3a84c1f92341bc62d12a404a0337fd069e3
Alex Birkett <alex@birkett.co.uk>
2013-05-01 21:34:13 +0200

    Run tests with different slice lengths between 1 and 10

--- protocol/gotop/test/up-message-test.js
+++ protocol/gotop/test/up-message-test.js
@@ -286,7 +286,7 @@ var createTests = function(sliceLength) {
 var suite = vows.describe('gotop-up-message-tests');
 
 var batch = {};
-for (var i = 0; i < 40; i++) {
+for (var i = 0; i < 10; i++) {
     batch['test with slice length ' + i] = createTests(i + 1);
 }
 suite.addBatch(batch);
