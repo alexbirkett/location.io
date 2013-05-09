c824d1f87b6b7f5febe04a1e99ff8f62edf5c726
Alex Birkett <alex@birkett.co.uk>
2013-05-09 19:33:59 +0200

    Remove unused file

--- protocol/tk103/command-builder.js
+++
@@ -1,8 +0,0 @@
-var capabilities = require('./api');
-var util = require('../util');
-var constants = require('./constants');
-
-var buildCommand = function(commandName, commandParameters) {	
-	util.assertValidCommand(commandName, commandParameters, capabilities);
-	return messageBuilders[commandName](commandParameters);
-};
