5799cab66820182b58b1aea89f86b32b191d1214
Alex Birkett <alex@birkett.co.uk>
2013-05-09 19:32:09 +0200

    No need to assert valid commands (already done by connection)

--- protocol/gotop/command-builder.js
+++ protocol/gotop/command-builder.js
@@ -1,9 +1,7 @@
 var capabilities = require('./api');
 var util = require('../util');
 
-
 var buildCommand = function(commandName, commandParameters) {	
-	util.assertValidCommand(commandName, commandParameters, capabilities);
 	return messageBuilders[commandName](commandParameters);
 };
 
