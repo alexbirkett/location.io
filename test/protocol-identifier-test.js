62e938d9a279d3738a281dd3e90e01407ed624c7
Alex Birkett <alex@birkett.co.uk>
2013-05-21 16:34:09 +0200

    Fix jshint errors

--- test/protocol-identifier-test.js
+++ test/protocol-identifier-test.js
@@ -8,41 +8,41 @@ var LocationIo = require('../index');
 
 var testHelper = require('../protocol/test/test-helper.js');
 
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err.stack);
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err.stack);
 });
 
 var nextPort = 2988;
 
-var getNextPort = function() {
-   return nextPort++; 
+var getNextPort = function () {
+    return nextPort++;
 };
 
-var testUpMessage = function() {
-   var args = [getNextPort];
-   args = args.concat(Array.prototype.slice.call(arguments, 0));
-   
-   var callback = args[args.length - 1];
-   
-   testHelper.testUpMessage.apply(this, args);
-}
+var testUpMessage = function () {
+    var args = [getNextPort];
+    args = args.concat(Array.prototype.slice.call(arguments, 0));
+
+    var callback = args[args.length - 1];
+
+    testHelper.testUpMessage.apply(this, args);
+};
 
 vows.describe('protocol-identifier-tests').addBatch({
     'handles gotop message': {
-        topic: function() {
-        	testUpMessage(GOTTOPMESSAGE, 0, 2, this.callback);
+        topic: function () {
+            testUpMessage(GOTTOPMESSAGE, 0, 2, this.callback);
         },
         'should be gotop message': function (err, message, returnedData, protocol) {
-			assert.equal(protocol, "gotop");
+            assert.equal(protocol, "gotop");
         }
     },
-   'handles tk103 message': {
-        topic: function() {
-        	testUpMessage(TK103MESSAGE, 0, 2, this.callback);	
+    'handles tk103 message': {
+        topic: function () {
+            testUpMessage(TK103MESSAGE, 0, 2, this.callback);
         },
         'should be gotop message': function (err, message, returnedData, protocol) {
-			assert.equal(protocol, "tk103");
+            assert.equal(protocol, "tk103");
         }
     }
-  
+
 }).export(module); // Export the Suite
