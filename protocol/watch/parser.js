6d65b9c36b06fa66d7313e94d4406874a319d8fd
Alex Birkett <alex@birkett.co.uk>
2013-05-21 17:00:39 +0200

    Fix jshint errors

--- protocol/watch/parser.js
+++ protocol/watch/parser.js
@@ -6,16 +6,16 @@ var lookupMessage = require('./message-types');
 var executeParseFunctionAndCatchException = util.executeParseFunctionAndCatchException;
 
 var POUND_CHARACTER_CODE = "#".charCodeAt(0);
-var messageLength = function(buffer) {
-	
-	var result = -1;
-	if (buffer.length > 0) {
-	    if (buffer[0] != POUND_CHARACTER_CODE) {
-	        throw "message must start with #";
-	    }
-	       
+var messageLength = function (buffer) {
+
+    var result = -1;
+    if (buffer.length > 0) {
+        if (buffer[0] != POUND_CHARACTER_CODE) {
+            throw "message must start with #";
+        }
+
         var index = util.bufferIndexOf(buffer, POUND_CHARACTER_CODE, 1, 9);
-        
+
         if (index > 0) {
             index++;
             if (buffer.length > index) {
@@ -26,46 +26,47 @@ var messageLength = function(buffer) {
                 }
             }
         }
-	}
-	
-	return result;
-}
+    }
 
-var parseMessage = function(buffer) {
+    return result;
+};
+
+var parseMessage = function (buffer) {
     var message = {};
     var startIndex = 1;
     var endIndex;
-    
-    var readNextValue = function() {
+
+    var readNextValue = function () {
         endIndex = util.bufferIndexOf(buffer, POUND_CHARACTER_CODE, startIndex);
         var string = buffer.toString('UTF8', startIndex, endIndex);
         startIndex = endIndex + 1;
         return string;
     };
- 
+
     message.imei = readNextValue();
-    message.userName = readNextValue(); 
+    message.userName = readNextValue();
     message.condition = readNextValue();
     message.password = readNextValue();
-    message.rawType  = readNextValue();
+    message.rawType = readNextValue();
     message.type = lookupMessage(message.rawType);
     message.number = readNextValue();
     message.validity = readNextValue();
     var location = readNextValue();
     message.location = parseGpsMessage(location);
-    message.networkOne = readNextValue()
+    message.networkOne = readNextValue();
     message.networkTwo = readNextValue();
     message.trackerId = message.userName + message.imei;
-    
+
     return message;
-}
-var findFrameAndParseMessage = function(buffer, callback) {
-	var error = null;
+};
+
+var findFrameAndParseMessage = function (buffer, callback) {
+    var error = null;
     var message;
-        
+
     var bufferToReturn = buffer;
-   
-   try {
+
+    try {
         var length = messageLength(buffer);
         if (length > 0) {
             message = parseMessage(buffer);
@@ -74,10 +75,10 @@ var findFrameAndParseMessage = function(buffer, callback) {
     } catch (e) {
         error = e;
     }
-   
-	setImmediate(function() {
+
+    setImmediate(function () {
         callback(error, message, bufferToReturn);
     });
-}
+};
 
 module.exports = findFrameAndParseMessage;
