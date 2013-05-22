a26e29eb88aa4fb3ef6d9b3a71551048f9129910
Alex Birkett <alex@birkett.co.uk>
2013-05-22 11:01:15 +0200

    Fix jshint errors

--- protocol/watch/api.js
+++ protocol/watch/api.js
@@ -1,18 +1,17 @@
-
 var passwordParameter = {
-    type : "text",
-    pattern : "^[0-9]{4}$"
-}; 
+    type: "text",
+    pattern: "^[0-9]{4}$"
+};
 
 var capabilities = {
-	message : {},
-	commands : {
-	    locateOneTime : {
-            parameters : {
+    message: {},
+    commands: {
+        locateOneTime: {
+            parameters: {
                 password: passwordParameter
             }
         }
-	}
-}; 
+    }
+};
 
 module.exports = capabilities;
