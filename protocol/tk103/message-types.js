4db45fb36b5f74fe0ad0fa51590ae62461228085
Alex Birkett <alex@birkett.co.uk>
2013-05-22 11:26:02 +0200

    Fix jshint errors

--- protocol/tk103/message-types.js
+++ protocol/tk103/message-types.js
@@ -1,6 +1,5 @@
-
 var messageTypes = {
-    
+
     'P00': 'handshakeSignalMessage',
     'P05': 'loginMessage',
     'S08': 'configureUpdateInterval',
@@ -14,28 +13,28 @@ var messageTypes = {
     'T00': 'restartDevice',
     'R05': 'configureUpdateIntervalWhenAccOpen',
     'R06': 'configureUpdateIntervalWhenAccClosed',
-    'U00': 'configureGeofence',
- /*   'R03': ['obtainTheTerminalLocationMessage', gpsMessageOnlyParse, 'AR03'],
-    'S20': ['responseToMonitoringCommands'],
-    'P02': ['answerToSettingUpTheTerminalIPAddressAndPort'],
-    'P03': ['answerToSettingAPNMessage'],
-    'P01': ['responseToReadingTheTerminalVersionMessage', responseToReadingTheTerminalVersionMessage],
-    'S21': ['responseToCancelingAllAlarmMessages'], 
-    'S04': ['answerToClearingMileageMessages'],
-    'S05': ['answerToStartingTheUpgradeMessages'],
-    'S06': ['answerToInitializeMileageMessage'],
-    'S23': ['answerToCenterSendsShortMessagesToThedispatchingScreen'],
-    'R04': ['dispatchScreenSendsAShortMessageToTheCenter', dispatchScreenSendsAShortMessageToTheCenterParse, 'AS07'],
-    'S09': ['responseToCenterSendAnInstantMessageToTheAdvertisingScreen'],
-    'R01': ['compensationDataReturnMessages', gpsMessageOnlyParse],
-    'Y01': ['answerToRequestPhotoTakingMessages', notImplemented],
-    'Y02': ['sendThePictureDataPacketsMessages', notImplemented],
-    'P16': ['answerToDownloadingGroupNumbers', answerToGroupNumbersParse],
-    'P17': ['answerToCancelingGroupNumbers', answerToGroupNumbersParse],
-    'P18': ['uploadGroupNumbers',uploadGroupNumbersParse, 'AP19'],
-    'O02': ['alarmForDataOffsetAndMessagesReturn', parseAlarmMessage]*/
+    'U00': 'configureGeofence'
+    /*   'R03': ['obtainTheTerminalLocationMessage', gpsMessageOnlyParse, 'AR03'],
+     'S20': ['responseToMonitoringCommands'],
+     'P02': ['answerToSettingUpTheTerminalIPAddressAndPort'],
+     'P03': ['answerToSettingAPNMessage'],
+     'P01': ['responseToReadingTheTerminalVersionMessage', responseToReadingTheTerminalVersionMessage],
+     'S21': ['responseToCancelingAllAlarmMessages'],
+     'S04': ['answerToClearingMileageMessages'],
+     'S05': ['answerToStartingTheUpgradeMessages'],
+     'S06': ['answerToInitializeMileageMessage'],
+     'S23': ['answerToCenterSendsShortMessagesToThedispatchingScreen'],
+     'R04': ['dispatchScreenSendsAShortMessageToTheCenter', dispatchScreenSendsAShortMessageToTheCenterParse, 'AS07'],
+     'S09': ['responseToCenterSendAnInstantMessageToTheAdvertisingScreen'],
+     'R01': ['compensationDataReturnMessages', gpsMessageOnlyParse],
+     'Y01': ['answerToRequestPhotoTakingMessages', notImplemented],
+     'Y02': ['sendThePictureDataPacketsMessages', notImplemented],
+     'P16': ['answerToDownloadingGroupNumbers', answerToGroupNumbersParse],
+     'P17': ['answerToCancelingGroupNumbers', answerToGroupNumbersParse],
+     'P18': ['uploadGroupNumbers',uploadGroupNumbersParse, 'AP19'],
+     'O02': ['alarmForDataOffsetAndMessagesReturn', parseAlarmMessage]*/
 };
 
-module.exports = function(messageCode) {
+module.exports = function (messageCode) {
     return messageTypes[messageCode];
-}
+};
