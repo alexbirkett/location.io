9bd47645603c41601096d93383c4e3d0933e4d28
Alex Birkett <alex@birkett.co.uk>
2013-05-22 11:32:30 +0200

    Fix jshint errors

--- protocol/tk103/message-parsers.js
+++ protocol/tk103/message-parsers.js
@@ -1,45 +1,45 @@
 var parseGpsMessage = require('./gps-message-parser');
 
-var gpsMessageOnlyParse = function(message, frame) {
+var gpsMessageOnlyParse = function (message, frame) {
     frame.location = parseGpsMessage(message);
 };
 
 var alarmTypes = {
-    '0' : 'vehiclePowerOff',
-    '1' : 'accident',
-    '2' : 'sos',
-    '3' : 'vehicleAntiTheftAndAlarming',
-    '4' : 'lowSpeedAlert',
-    '5' : 'overSpeedAlert',
-    '6' : 'geoFence',
-    '7' : 'movementAlert'
+    '0': 'vehiclePowerOff',
+    '1': 'accident',
+    '2': 'sos',
+    '3': 'vehicleAntiTheftAndAlarming',
+    '4': 'lowSpeedAlert',
+    '5': 'overSpeedAlert',
+    '6': 'geoFence',
+    '7': 'movementAlert'
 };
 
 module.exports = {
-    messages : {
-        handshakeSignalMessage : function(message, frame) {
+    messages: {
+        handshakeSignalMessage: function (message, frame) {
             var handshakeSignalMessagePattern = /(.*)HSO/;
             var matchArray = handshakeSignalMessagePattern.exec(message);
             frame.serialNumber = matchArray[1];
         },
-        alarmMessage : function(message, frame) {
+        alarmMessage: function (message, frame) {
             var alarmMessagePattern = /(.)(.*)/;
             var matchArray = alarmMessagePattern.exec(message);
             frame.alarmType = alarmTypes[matchArray[1]];
             frame.location = parseGpsMessage(matchArray[2]);
         },
-        loginMessage : function(message, frame) {
-           var loginMessagePattern = /(.{15})(.*)/;
-           var matchArray = loginMessagePattern.exec(message);
-           frame.terminalId = matchArray[1];
-           frame.location = parseGpsMessage(matchArray[2]); 
+        loginMessage: function (message, frame) {
+            var loginMessagePattern = /(.{15})(.*)/;
+            var matchArray = loginMessagePattern.exec(message);
+            frame.terminalId = matchArray[1];
+            frame.location = parseGpsMessage(matchArray[2]);
         },
-        locationUpdate: function(message, frame) {
-           gpsMessageOnlyParse(message, frame);
+        locationUpdate: function (message, frame) {
+            gpsMessageOnlyParse(message, frame);
         }
     },
-    acks : {
-        configureUpdateInterval : function(message) {
+    acks: {
+        configureUpdateInterval: function (message) {
             var regEx = /(.{4})(.{2})(.{2})/;
 
             var frame = {};
@@ -50,43 +50,40 @@ module.exports = {
             frame.minutes = matchArray[3];
             return frame;
         },
-        requestLocation : function(message, frame) {
+        requestLocation: function (message, frame) {
             gpsMessageOnlyParse(message, frame);
         },
-        configureSpeedAlert : function(message, frame) {
+        configureSpeedAlert: function (message, frame) {
             var responseToSetUpVehicleMaxAndMinSpeedMessagePattern = /H(.*)L(.*)/;
             var matchArray = responseToSetUpVehicleMaxAndMinSpeedMessagePattern.exec(message);
             // this is guesswork, the document does not specify what H and L are.
             frame.maxSpeed = matchArray[1];
             frame.minSpeed = matchArray[2];
         },
-        requestLocation : function(message, frame) {
+        updatesEnding: function (message, frame) {
             gpsMessageOnlyParse(message, frame);
         },
-        updatesEnding : function(message, frame) {
-            gpsMessageOnlyParse(message, frame);
-        },
-        configureSwitch0 : function(message, frame) {
-            frame.enabled = (message  == '1');
-        },
-        configureSwitch1 : function(message, frame) {
+        configureSwitch0: function (message, frame) {
             frame.enabled = (message == '1');
         },
-        restartDevice : function(message, frame) {
-           // no implementation requred
-        }, 
-        configureUpdateIntervalWhenAccOpen : function(message, frame) {
-           // no implementation requred
-        }, 
-        configureUpdateIntervalWhenAccClosed : function(message, frame) {
+        configureSwitch1: function (message, frame) {
+            frame.enabled = (message == '1');
+        },
+        restartDevice: function (message, frame) {
+            // no implementation requred
+        },
+        configureUpdateIntervalWhenAccOpen: function (message, frame) {
+            // no implementation requred
+        },
+        configureUpdateIntervalWhenAccClosed: function (message, frame) {
             console.log('configureUpdateIntervalWhenAccClosed ack parse not implemented');
             console.log(message);
-        }, 
-        configureGeofence : function(message, frame) {
+        },
+        configureGeofence: function (message, frame) {
             var geoFenceMessageTypes = {
-                '0':'cancelOutsideFence',
-                '1':'outsideFence',
-                '2':'insideFence'
+                '0': 'cancelOutsideFence',
+                '1': 'outsideFence',
+                '2': 'insideFence'
             };
             frame.geoFenceResponseType = geoFenceMessageTypes[message[0]];
         }
