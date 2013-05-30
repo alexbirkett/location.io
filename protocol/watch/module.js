a7415ca9aadeaf823677f10840e489608bbe69f2
Alex Birkett <alex@birkett.co.uk>
2013-05-30 11:37:32 +0200

    Remove watch protocol down messages (watch protocol does not support down messages)

--- protocol/watch/module.js
+++ protocol/watch/module.js
@@ -1,7 +1,3 @@
-var messageBuilder = require('./message-builder');
-
 module.exports = {
-    api: require('./api'),
-    parseMessage: require('./parser'),
-    buildMessage: messageBuilder.buildMessage
+    parseMessage: require('./parser')
 };
